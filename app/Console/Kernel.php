ed98a2e62b290db9dc3a38270c0f84b264fd44d0
Martijn Katerbarg <martijnkaterbarg@gmail.com>
2024-12-27 17:44:48 +0100

    fix: Enable rescan rss command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Console;
 
 use App\Console\Commands\RescanLibrary;
+use App\Console\Commands\RescanRssFeeds;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -14,7 +15,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        RescanLibrary::class
+        RescanLibrary::class,
+        RescanRssFeeds::class
     ];
 
     /**
